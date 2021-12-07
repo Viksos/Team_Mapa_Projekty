53dec890284664b42ef6adb97a55d96a09c1c8ce
Hasang4 <31667070+Hasang4@users.noreply.github.com>
2021-12-07 21:26:59 +0100

    Update Data_analys.py

--- Data_analys.py
+++ Data_analys.py
@@ -22,3 +22,32 @@ def data_to_model(df):
 
 	return(X_train, X_test, y_train, y_test)
 
+# heatmap of correlation
+
+corr = df.iloc[:, 1:7].corr()
+heatmap = sns.heatmap(corr[['y']].sort_values(by = 'y', ascending = False), vmin = 0, vmax = 1, annot = True, cmap = 'winter')
+heatmap.set_title('Features Correlating with "y" variable', fontdict={'fontsize':14}, pad=15);
+print(corr.iloc[[5]])
+
+zmienne = corr.iloc[[5]].to_numpy()
+zmienne = np.delete(zmienne, -1)
+print(zmienne)
+
+for i,v in enumerate(zmienne):
+	print('Feature:', i,', Correlation:',v)
+
+plt.bar([x for x in range(len(zmienne))], zmienne)
+plt.ylabel("Correlation")
+plt.xlabel("Features")
+plt.show()
+
+print(zmienne)
+
+# Important features
+
+important_features = []
+for i in range(zmienne.size):
+    if zmienne[i]>0.1:
+        important_features.append(i)
+
+print(important_features)
